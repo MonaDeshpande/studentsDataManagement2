3931ff2374b4b2631fd507233ca82c8dcd5a5cae
MonaDeshpande <101972805+MonaDeshpande@users.noreply.github.com>
2023-10-18 15:34:54 +0530

    commit

--- authentication/views.py
+++ authentication/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("I am working")
+    #return HttpResponse("I am working")
+    return render(request, "authentication/home.html", {})
 
 def signup(request):
+    #print("processing signup")
     return render(request, "authentication/signup.html")
 
 def signin(request):
+    #print("processing")
     return render(request, "authentication/signin.html")
 
 def signout(request):
